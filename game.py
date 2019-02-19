627c14e19af3307078d49fb1b0dc85509fd0276b
João Graça <jonymiguelgraca@gmail.com>
2019-02-19 14:08:35 +0000

    Adds interaction between user and game

--- game.py
+++ game.py
@@ -4,25 +4,14 @@ from random import choice, randint
 
 from view.board import Board
 
-class Position:
-    def __init__(self, column, row):
-        self.column = column
-        self.row = row
-
-    def __str__(self):
-        return str(self.__dict__)
-
-    def __eq__(self, other): 
-        return self.__dict__ == other.__dict__
-
 class Game:
     
     @staticmethod
     def create_board_with_size(size):
-        result = [None] * size
+        rows = [None] * size
         for i in range(size):
-            result[i] = [0] * size
-        return result
+            rows[i] = [0] * size
+        return rows
 
     def __init__(self, size):
         self.board = Game.create_board_with_size(size)
@@ -32,30 +21,97 @@ class Game:
         #print (self.board)
 
     def insert_random_number_in_board(self):
-        position = choice(self.freePositions)
+        if len(self.freePositions) == 0:
+            #TODO: Draw Loser
+            pass
+        (row, column) = choice(self.freePositions)
 
-        self.board[position.row][position.column] = 2
-        self.freePositions.remove(position)
+        self.board[row][column] = 2
+        self.freePositions.remove((row, column))
 
     def getFreePositions(self):
         positions = []
         for row, columns in enumerate(self.board):
             for column, value in enumerate(columns):
                 if value == 0:
-                    positions.append(Position(column, row))
+                    positions.append((row, column))
         return positions
 
+    @staticmethod
+    def moveLeft(columns):
+        current_value = -1
+        current_value_pos = -1
+        positions_to_delete = []
+        for column, value in enumerate(columns):
+            if value == 0:
+                positions_to_delete.insert(0, column)
+                continue
+            if current_value != value:
+                current_value = value
+                current_value_pos = column
+                continue
+            else:
+                columns[current_value_pos] += value
+                positions_to_delete.insert(0, column)
+                current_value = -1
+                current_value_pos = -1
+        for column in positions_to_delete: 
+            del columns[column]
+            columns.append(0)
+
+    def transposeBoard(self):
+        return [[self.board[j][i] for j in range(len(self.board))] for i in range(len(self.board[0]))]
+
     def run(self):
-        view = Board(self.board)
+        print(self.board)
+        view = Board(lambda: self.board)
         view.draw_board()
 
         while True:
             key = view.window.getch()
             if key == ord('q'):
                 break
+            if key in [KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT]:
+                if key == KEY_LEFT:
+                    for _, columns in enumerate(self.board):
+                        Game.moveLeft(columns)
+                elif key == KEY_RIGHT:
+                    for _, columns in enumerate(self.board):
+                        columns.reverse()
+                        Game.moveLeft(columns)
+                        columns.reverse()
+                elif key == KEY_UP:
+                    self.board = self.transposeBoard()
+                    for _, columns in enumerate(self.board):
+                        Game.moveLeft(columns)
+                    self.board = self.transposeBoard()
+                elif key == KEY_DOWN:
+                    self.board = self.transposeBoard()
+                    for _, columns in enumerate(self.board):
+                        columns.reverse()
+                        Game.moveLeft(columns)
+                        columns.reverse()
+                    self.board = self.transposeBoard()
+
+
+
+                #TODO Optimize this
+                self.freePositions = self.getFreePositions()
+                self.insert_random_number_in_board()
+                view.draw_board()
 
         curses.endwin()
 
 
 if __name__ == '__main__':
+    """ a = [2, 2, 4, 4]
+    Game.moveLeft(a)
+    a.reverse()
+    print(a) """
+    
+    """ matrix = [[1, 2, 3],[4, 5, 6], [7, 8, 9]]
+    print(matrix)
+    matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    print(matrix) """
+    
     Game(4).run()
--- view/board.py
+++ view/board.py
@@ -1,29 +1,30 @@
-import sys
 import curses
-import time
 import os
+import sys
+import time
 
 class Board:
     column_width = 6
     blank_column_line = "{}|".format(" " * column_width)
     column_divider = "{}+".format("-" * column_width)
 
-    def __init__(self, board):
-        self.board = board
+    def __init__(self, boardSupplier):
+        self.boardSupplier = boardSupplier
+        board = boardSupplier()
         
-        self.divider = "\r+{0}".format(self.column_divider * len(self.board))
-        self.column_separators = "\r|{0}".format(self.blank_column_line * len(self.board))
+        self.divider = "\r+{0}".format(self.column_divider * len(board))
+        self.column_separators = "\r|{0}".format(self.blank_column_line * len(board))
         self.width = len(self.column_separators)
 
-        stdscr = curses.initscr()
+        curses.initscr()
         self.window = curses.newwin(20, self.width, 0, 0)
+        self.window.keypad(1)
         curses.noecho()
-        stdscr.keypad(True)
     
     def draw_board(self):
         """ It will (re)print the string representation of the board """
-        x = 0
-        for _, columns in enumerate(self.board):
+        x = 1
+        for _, columns in enumerate(self.boardSupplier()):
             self.window.addstr(x, 0, self.divider)
             self.window.addstr(x + 1, 0, self.column_separators)
             self.draw_board_line_with_value(x + 2, columns)
@@ -31,7 +32,7 @@ class Board:
             x += 4
         self.window.addstr(x, 0, self.divider)
 
-        self.window.addstr(x + 2, 0, "Q - exit")
+        self.window.addstr(x + 2, 0, "Q - exit; Arrows for movement")
 
     def draw_board_line_with_value(self, x, columns):
         line = "|"
@@ -42,4 +43,3 @@ class Board:
                 space_remainder = self.column_width - len(str(num))
                 line += "{0}{1}{2}|".format(" " * (space_remainder//2 + space_remainder % 2), num, " " * (space_remainder//2))
         self.window.addstr(x, 0, line)
-        
